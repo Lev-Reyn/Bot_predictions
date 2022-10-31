7bc2d72564206f13f5fea1d8fddb33ffb5a45750
Lev <83927772+Lev-Reyn@users.noreply.github.com>
2022-10-31 04:19:13 +0300

    version 1.0

---
+++ main.py
@@ -0,0 +1,56 @@
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+from aiogram.utils.exceptions import BotBlocked
+from config import token
+from work_with_data_users.work_with_data_users import WorkWithDataUsers
+import aioschedule
+
+bot = Bot(token, parse_mode=types.ParseMode.HTML)
+dp = Dispatcher(bot)
+import asyncio
+
+
+@dp.message_handler(commands=['start'])
+async def start_process_command(message: types.Message):
+    """начало работы бота"""
+    await message.delete()
+    WorkWithDataUsers(message.from_user.id)
+    await bot.send_message(message.from_user.id, 'Привет, я предсказываю будущее, и если тебе не похуй, то забей'
+                                                 ' хуй и раз в день получай эту хуйню, ведь это точно правдивые '
+                                                 'предсказания!!!')
+
+
+@dp.message_handler(commands=['send_prediction'])
+async def start_process_command(message: types.Message):
+    """отправляет всем зареганым предсказание"""
+    for user_telegram_id in WorkWithDataUsers(message.from_user.id).get_all_users():
+        try:
+            await bot.send_message(user_telegram_id, WorkWithDataUsers(user_telegram_id).get_prediction())
+        except BotBlocked:
+            # нужно добавлять в список тех, кто остановил бота, и возможно потом удалять их, хз
+            print(f'пользователь с telegram_id {user_telegram_id} остановил бота')
+
+
+@dp.message_handler()
+async def start_process_command_no_command():
+    """отправляет всем зареганым предсказание"""
+    for user_telegram_id in WorkWithDataUsers('664295561').get_all_users():
+        try:
+            await bot.send_message(user_telegram_id, WorkWithDataUsers(user_telegram_id).get_prediction())
+        except BotBlocked:
+            # нужно добавлять в список тех, кто остановил бота, и возможно потом удалять их, хз
+            print(f'пользователь с telegram_id {user_telegram_id} остановил бота')
+
+
+async def scheduler(m):
+    """позволяет запустить функцию в определённое время"""
+    aioschedule.every().day.at("03:47").do(start_process_command_no_command)
+    while True:
+        await aioschedule.run_pending()
+        await asyncio.sleep(1)
+
+
+if __name__ == '__main__':
+    # executor.start_polling(dp, on_startup=scheduler)
+    executor.start_polling(dp, skip_updates=True, on_startup=scheduler)
---
+++ work_with_data_users/work_with_data_users.py
@@ -0,0 +1,43 @@
+import os
+from in_json import InJson
+from random import choice
+
+
+class WorkWithDataUsers(InJson):
+    def __init__(self, telegram_id: str):
+        self.telegram_id = telegram_id
+        super().__init__(f'data_users/{self.telegram_id}user.json')
+
+    def get_all_users(self):
+        """получить все telegram_id юзеров"""
+        users_json = os.listdir('data_users')  # названия файлов юзеров
+        users_telegram_id = []
+        for user in users_json:
+            users_telegram_id.append(user.replace('user.json', ''))
+        return users_telegram_id
+
+    def add_prediction_in_user_file(self, prediction):
+        """добавляет отправленное предсказание в файл юзера и возвращает обратно это же предсказание"""
+        super().update_list([prediction])
+        return prediction
+
+    def get_prediction(self):
+        """получить предсказание для этого пользователя (для каждого то, которого ещё человек не получал),
+        ну и вызывает метод add_prediction_in_user_file"""
+        super().__init__('data_predictions/predictions.json')
+        predictions = super().reed_json()
+        predictions: list
+        super().__init__(f'data_users/{self.telegram_id}user.json')
+        user_old_predictions = super().reed_json()
+        for need_del in user_old_predictions:
+            predictions.remove(need_del)
+        if len(predictions) == 0:
+            return 'на сегодня пиздец, нет у меня для тебя предсказания :('
+        return self.add_prediction_in_user_file(choice(predictions))
+
+
+if __name__ == '__main__':
+    os.chdir('../')  # поставить основную директорию другую (в данном случа я поставил bot_predictions
+    # в качестве основной)
+    dima = WorkWithDataUsers('664295561')
+    print(dima.get_prediction())
